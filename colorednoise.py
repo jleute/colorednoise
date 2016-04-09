475ae39436918ce205d74e04d17d32f08eab56a9
Anders Wallin <anders.e.e.wallin@gmail.com>
2016-04-09 12:02:07 +0300

    functions for phase-psd, frequency-psd, and ADEV from noiseGen parameters

--- colorednoise.py
+++ colorednoise.py
@@ -6,6 +6,7 @@ Kasdin, N.J., Walter, T., "Discrete simulation of power law noise
 [for oscillator stability evaluation]," 
 Frequency Control Symposium, 1992. 46th., Proceedings of the 1992 IEEE, 
 pp.274,283, 27-29 May 1992
+http://dx.doi.org/10.1109/FREQ.1992.270003
 '''
 
 # author: Julia Leute
@@ -36,3 +37,68 @@ def noiseGen(nr, Qd, b):
     # inverse Fourier transform
     colorednoise = np.fft.irfft(np.fft.rfft(wfb)*np.fft.rfft(hfb))[:nr]
     return colorednoise
+
+def phase_psd_from_qd(qd, b, tau0):
+    """ return phase power spectral density coefficient g_b from QD
+    
+        Colored noise generated with (qd, b, tau0) parameters will
+        show a phase power spectral density of
+        S_x(f) = Phase_PSD(f) = g_b * f^b
+        
+        Kasdin & Walter eqn (39)
+    """
+    g_b = qd*2.0*pow(2.0*math.pi, b)*pow(tau0, b+1.0)
+    return g_b
+
+def frequency_psd_from_qd(qd, b, tau0):
+    """ return frequency power spectral density coefficient h_a from QD
+        
+        Colored noise generated with (qd, b, tau0) parameters will
+        show a frequency power spectral density of
+        
+        S_y(f) = Frequency_PSD(f) = h_a * f^a
+        where the slope a comes from the phase PSD slope b:
+        a = b + 2
+        
+        Kasdin & Walter eqn (39)
+    """
+    a = b + 2.0
+    h_a = qd*2.0*pow(2.0*math.pi, a)*pow(tau0, a-1.0)
+    return h_a
+    
+def adev_from_qd(qd, b, tau0):
+    """ prefactor for Allan deviation from QD and slope
+    
+        Colored noise generated with (qd, b, tau0) parameters will
+        show an Allan variance of:
+        
+        AVAR = prefactor * h_a * tau^c
+        
+        where a = b+2 is the slope of the frequency PSD.
+        and h_a is the frequency PSD prefactor S_y(f) = h_a * f^a
+        
+        The relation between a, b, c is:
+        a   b   c(AVAR) c(MVAR)
+        -----------------------
+        -2  -4   1       1
+        -1  -3   0       0
+         0  -2  -1      -1
+        +1  -1  -2      -2
+        +2  -2  -2      -3
+    
+        Letters A..E are from IEEE1139-2008, Table B.2
+    """
+    g_b = phase_psd_from_qd(qd,b,tau0)
+    f_h = 1.0
+    if b == 0:
+        coeff = 3.0*f_h / (4.0*pow(math.pi,2)) # E, White PM, tau^-1
+    elif b == -1:
+        coeff = (1.038+3.0*math.log(2*math.pi*f_h*tau0))/(4.0*pow(math.pi,2)) # D, Flicker PM, tau^-1
+    elif b == -2:
+        coeff = 0.5 # C, white FM,  1/sqrt(tau)
+    elif b == -3:
+        coeff = 2*math.log(2) # B, flicker FM,  constant ADEV
+    elif b == -4:
+        coeff = 2.0*pow(math.pi,2)/3.0 #  A, RW FM, sqrt(tau)
+
+    return math.sqrt(coeff*g_b*(2.0*pow(math.pi,2)))
